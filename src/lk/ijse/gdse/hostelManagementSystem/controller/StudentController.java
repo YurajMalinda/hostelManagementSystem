371a643709c850cf71105f039e9fb7712d02828f
YurajMalinda <yurajmalinda123.com>
2023-09-04 21:41:10 +0530

    Dashboard UI modified and student manage UI created.

---
+++ src/lk/ijse/gdse/hostelManagementSystem/controller/StudentController.java
@@ -0,0 +1,52 @@
+package lk.ijse.gdse.hostelManagementSystem.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
+
+public class StudentController {
+    public JFXButton btnEdit;
+    public JFXButton btnDelete;
+    public JFXButton btnNew;
+    public JFXTextField txtAddress;
+    public JFXTextField txtID;
+    public JFXTextField txtName;
+    public JFXTextField txtContact;
+    public RadioButton rbFemale;
+    public ToggleGroup gender;
+    public RadioButton rbMale;
+    public JFXDatePicker txtDOB;
+    public JFXButton btnCancel;
+    public JFXButton btnSave;
+    public JFXTextField txtSearch;
+    public TableView tblStudent;
+    public TableColumn colID;
+    public TableColumn colName;
+    public TableColumn colAddress;
+    public TableColumn colContact;
+    public TableColumn colDOB;
+    public TableColumn colGender;
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEditOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
