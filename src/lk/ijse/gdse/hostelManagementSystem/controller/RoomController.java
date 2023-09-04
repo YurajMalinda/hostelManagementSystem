fc63044d94bd3101828ac50bce12fa64e92e3c11
YurajMalinda <yurajmalinda123.com>
2023-09-05 00:27:41 +0530

    Student UI modified and Room UI created.

---
+++ src/lk/ijse/gdse/hostelManagementSystem/controller/RoomController.java
@@ -0,0 +1,43 @@
+package lk.ijse.gdse.hostelManagementSystem.controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class RoomController {
+    public JFXTextField txtSearch;
+    public TableView tblRooms;
+    public TableColumn colRoomTypeID;
+    public TableColumn colType;
+    public TableColumn colKeyMoney;
+    public TableColumn colQty;
+    public JFXButton btnEdit;
+    public JFXButton btnDelete;
+    public JFXButton btnNew;
+    public JFXTextField txtKeyMoney;
+    public JFXTextField txtRoomTypeID;
+    public JFXTextField txtType;
+    public JFXTextField txtQty;
+    public JFXButton btnCancel;
+    public JFXButton btnSave;
+
+    public void btnBackOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnEditOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnNewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnCancelOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSaveOnAction(ActionEvent actionEvent) {
+    }
+}
